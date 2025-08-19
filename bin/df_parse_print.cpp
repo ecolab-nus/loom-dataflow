2c8c3f72a5258781293974d928487a9329ef36ec
zhenyu bai <zhenyu.bai@nus.edu.sg>
2025-08-19 11:56:30 +0000

    add a first version of the dataflow dialect

---
+++ bin/df_parse_print.cpp
@@ -0,0 +1,47 @@
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/IR/AsmState.h"
+#include "mlir/IR/BuiltinDialect.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Parser/Parser.h"
+#include "mlir/Support/FileUtilities.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/WithColor.h"
+
+#include "DataflowDialect.h.inc"
+
+using namespace mlir;
+
+int main(int argc, char **argv) {
+  MLIRContext context;
+  (void)context.getOrLoadDialect<mlir::BuiltinDialect>();
+  context.loadDialect<mlir::func::FuncDialect>();
+  context.loadDialect<mlir::memref::MemRefDialect>();
+  context.loadDialect<mlir::affine::AffineDialect>();
+  context.loadDialect<mlir::arith::ArithDialect>();
+  context.loadDialect<tmd::df::DataflowDialect>();
+
+  llvm::SourceMgr sourceMgr;
+  const char *filename = argc > 1 ? argv[1] : "-";
+  auto file = mlir::openInputFile(filename);
+  if (!file) {
+    llvm::WithColor::error(llvm::errs())
+        << "Failed to open input file: " << filename << "\n";
+    return 1;
+  }
+  sourceMgr.AddNewSourceBuffer(std::move(file), llvm::SMLoc());
+
+  OwningOpRef<ModuleOp> module = parseSourceFile<ModuleOp>(sourceMgr, &context);
+  if (!module) {
+    llvm::WithColor::error(llvm::errs()) << "Failed to parse MLIR module\n";
+    return 1;
+  }
+
+  AsmState asmState(*module);
+  module->print(llvm::outs(), asmState);
+  llvm::outs() << "\n";
+  return 0;
+}
---
+++ lib/scaleout/dataflow-dialect/DataflowDialect.cpp
@@ -0,0 +1,25 @@
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/MLIRContext.h"
+
+#include "DataflowDialect.h.inc"
+
+using namespace mlir;
+using namespace tmd::df;
+
+#include "DataflowDialect.cpp.inc"
+// Bring in op class declarations for registration below.
+#define GET_OP_CLASSES
+#include "DataflowOps.h.inc"
+
+void DataflowDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include "DataflowOps.cpp.inc"
+      >();
+}
