123791942769ec97f86b70d492e4d333bee9bce2
heru wang <heru.wang@nus.edu.sg>
2026-01-05 08:42:13 +0000

    feat: standalone hoisting pass

---
+++ tool/triton-shared/single_stage/hoist_block_loading_main.cpp
@@ -0,0 +1,80 @@
+// Standalone driver to run the hoist block loading pass.
+//
+// Usage:
+//   hoist_block_loading --input <input.mlir>
+//   hoist_block_loading --input -  (reads from stdin)
+
+#include "hoist_block_loading.h"
+
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Bufferization/IR/Bufferization.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/IR/AsmState.h"
+#include "mlir/IR/BuiltinDialect.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Parser/Parser.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Support/FileUtilities.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/WithColor.h"
+
+#include "DataflowDialect.h.inc"
+#include "DataflowOps.h.inc"
+
+using namespace mlir;
+
+static llvm::cl::opt<std::string>
+    clInput("input", llvm::cl::desc("Path to input MLIR file (use '-' for stdin)"),
+            llvm::cl::value_desc("filename"), llvm::cl::init("-"));
+
+int main(int argc, char **argv) {
+  llvm::cl::ParseCommandLineOptions(argc, argv,
+                                    "LOOM Triton-shared hoist block loading\n");
+
+  MLIRContext context;
+  context.loadDialect<mlir::BuiltinDialect>();
+  context.loadDialect<mlir::func::FuncDialect>();
+  context.loadDialect<mlir::affine::AffineDialect>();
+  context.loadDialect<mlir::arith::ArithDialect>();
+  context.loadDialect<mlir::memref::MemRefDialect>();
+  context.loadDialect<mlir::tensor::TensorDialect>();
+  context.loadDialect<mlir::linalg::LinalgDialect>();
+  context.loadDialect<mlir::scf::SCFDialect>();
+  context.loadDialect<mlir::bufferization::BufferizationDialect>();
+  context.loadDialect<loom::df::DataflowDialect>();
+
+  llvm::SourceMgr sm;
+  auto file = mlir::openInputFile(clInput);
+  if (!file) {
+    llvm::WithColor::error(llvm::errs())
+        << "Failed to open input file: " << clInput << "\n";
+    return 1;
+  }
+  sm.AddNewSourceBuffer(std::move(file), llvm::SMLoc());
+  OwningOpRef<ModuleOp> module = parseSourceFile<ModuleOp>(sm, &context);
+  if (!module) {
+    llvm::WithColor::error(llvm::errs()) << "Failed to parse MLIR module\n";
+    return 1;
+  }
+
+  PassManager pm(&context);
+  pm.addPass(loom::passes::createHoistBlockLoadingPass());
+  if (failed(pm.run(*module))) {
+    llvm::WithColor::error(llvm::errs()) << "Hoist block loading pass failed\n";
+    return 2;
+  }
+
+  mlir::OpPrintingFlags flags;
+  flags.useLocalScope();
+  module->print(llvm::outs(), flags);
+  llvm::outs() << "\n";
+  return 0;
+}
+
